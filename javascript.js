7bb92308d64c0d66a240c7c53bdc6e7c5c521b26
Kristen Wegner <klwegner@gmail.com>
2022-06-22 10:35:05 -0400

    simplified classes

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
+//to do: 1. fix crashwith; 2. reset stepmom and stone if not crash; 3. scream sound effect; 4. Randomize stepmom movement
+
+
+
+
+
+
 window.onload = () => {
   let totalFrameCount = 0;
   let intervalId = null;
@@ -8,6 +15,7 @@ window.onload = () => {
 
   document.querySelector(".start-button").onclick = () => {
     console.log("button clicked");
+
     // document.querySelector('canvas').focus();
 
     //make it where timer starts with click
@@ -26,64 +34,81 @@ window.onload = () => {
     stepmom.src = "./silhouette_wip.png";
     const millstone = new Image();
     millstone.src = "./millstone_done.png";
+    let momScream = new sound("scream11.wav");
+    let crowCaw = new sound("crow1.mp3")
+    crowCaw.play();
 
-    class MovingObjects {
-      constructor(x, y, width, height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.vY = 10;
-      }
 
-      draw() {
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-      }
-      left() {
-        return this.x;
-      }
-      right() {
-        return this.x + this.width;
-      }
 
-      top() {
-        return this.y;
-      }
+    var backgroundMusic = new Audio('./background muZAK.mp3');
+    backgroundMusic.loop = true;
+    backgroundMusic.volume = 0.4;
+    backgroundMusic.play();
 
-      bottom() {
-        return this.y + this.height;
-      }
+  
 
-      crashWith(target) {
-        return !(
-          this.bottom() < target.top() ||
-          this.right() < target.left() ||
-          this.left > target.right()
-        );
-      }
+    class ImageObject {
+      constructor(x, y, width, height, imageElement) {
+      this.x = x;
+      this.y = y;
+      this.width = width;
+      this.height = height;
+      this.vY = 10;
+      this.vX = 5;
+      this.image = imageElement;
+    }
 
-      updatePositionFall() {
- 
-        if(this.y <= 475) {
-          this.y += this.vY;
-        } else {
-          this.y == 475;
-        }
+    draw() {
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    left() {
+      return this.x;
+    }
+    right() {
+      return this.x + this.width;
+    }
 
+    top() {
+      return this.y;
+    }
+
+    bottom() {
+      return this.y + this.height;
+    }
+
+    crashWith(target) {
+      return !(
+        this.bottom() < (target.top()+35) ||
+        this.right() < target.left() ||
+        this.left > target.right()
+      );
+    }
+
+    updatePositionFall() {
+
+      if(this.y <= 475) {
+        this.y += this.vY;
+      } else {
+        this.y == 475;
       }
     }
 
-    class ImageObject extends MovingObjects {
-      constructor(x, y, width, height, imageElement) {
-        super(x, y, width, height);
-        this.image = imageElement;
-      }
       draw() {
         ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
       }
-      
+
+      updateStepmom() {
+        if ((this.x + 50) > 800 || this. x < 25){
+          this.vX *=-1;
+          this.x += this.vX;
+     } else {
+      this.x += this.vX;
+     }
     }
 
+    }
+     
+      
     let stepX = 400;
     let stepY = 500;
     let millX = 375;
@@ -93,37 +118,22 @@ window.onload = () => {
     const myStepmom = new ImageObject(stepX, stepY, 50, 100, stepmom);
     const myMillstone = new ImageObject(millX, millY, 50, 50, millstone);
 
-    // var x=5;
-    // var velocity = 10;
+  
 
-    //where to put this function???
-    // function stepmomMove(){
 
-    //   myCanvas.width = window.innerWidth;
-    //   myCanvas.height = window.innerHeight;
-    //   x = x + velocity;
-    //   if ((x+50)>myCanvas.width || x<0){
-    //     velocity *=-1;
-    //   }
-
-    //   ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-    //   myBackground.draw();
-    //   myMillstone.draw();
-    //   }
 
     function minTimer() {
       let remainingTime = totalSeconds - Math.floor(totalFrameCount / 60);
   
       console.log(`${remainingTime}`);
   
-      //remainingTime -= 1;
+
       place.textContent = `${remainingTime}`;
       if (remainingTime < 10) {
         place.style.color = "red";
       }
   
       if (remainingTime <= 0) {
-        //document.querySelector(".countdown").innerHTML = "0";
         clearInterval(intervalId);
       }
     }
@@ -133,22 +143,31 @@ window.onload = () => {
     function updateGame() {
       totalFrameCount++;
       minTimer();
+      myStepmom.updateStepmom();
+
+
+
+
+
+
       ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
       myBackground.draw();
       myStepmom.draw();
       myMillstone.draw();
 
-      // if(myMillstone.crashWith(myStepmom)){
-      //   clearInterval(intervalId);
-      //play
-      //   alert('The father and little Marleen heard the sound and ran out, but only saw mist and flame and fire. When these had passed, there stood the little brother ...')
+
+
+      if(myMillstone.crashWith(myStepmom)){
+        // momScream.play();
+        clearInterval(intervalId);
+        alert('The father and little Marlinchen heard the sound but saw only mist and fire. When these had passed, there stood the little brother... and all three rejoiced.')
+      }
+
     }
 
     background.onload = () => {
       intervalId = setInterval(updateGame, 16.67);
     };
-
-    //clicking repeatedly messes up timer
 
     document.addEventListener("keydown", (event) => {
 event.preventDefault();
@@ -156,8 +175,7 @@ event.preventDefault();
         case "Space":
           myMillstone.updatePositionFall();
           console.log("space pressed");
-          //let intervalId = setInterval(fallingStone, 500);
-
+      
           break;
         //case to reload stone
       }
@@ -172,22 +190,27 @@ event.preventDefault();
                 myStepmom.draw();
                 myMillstone.draw();
                 console.log("space released");
-                //let intervalId = setInterval(fallingStone, 500);
-      
                 break;
-              //case to reload stone
             }
           });
-
-    // myStepmom.draw();
   }
 
+}
 
-};
+
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = "./crow1.mp3";
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function(){
+      this.sound.play();
+  }
+  this.stop = function(){
+      this.sound.pause();
+  }    
+}
 
 
-// const music = new Audio('https://www.shockwave-sound.com/sound-effects/scream-sounds/scream11.wav');
-// music.play();
-// music.loop = false;
-// music.playbackRate = 2;
-// music.pause();qqazszdgfbgtyj
