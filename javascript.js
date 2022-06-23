ccd40ce100d35af8a5dfcd157b376b49012f8a44
Kristen Wegner <klwegner@gmail.com>
2022-06-23 14:54:20 -0400

    story images edited

--- javascript.js
+++ javascript.js
@@ -284,5 +284,5 @@ function showSlides() {
   }
   slideIndex++;
   slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 5000); // Change image every 2 seconds
+  setTimeout(showSlides, 10000); // Change image every 2 seconds
 }
